7918bdf23a26838760763c35062f22ce2265b639
shreyanshVIT23 <shreyansh.23bce10701@vitbhopal.ac.in>
2024-12-08 12:49:02 +0530

    Updated to error in nlp handling as well as import correction

--- server/Chatbot/audio_to_text.py
+++ server/Chatbot/audio_to_text.py
@@ -9,8 +9,8 @@ from nltk.tokenize import word_tokenize
 import nltk
 import os
 from pathlib import Path
-from Utils.svg_manipulator import main as make_svg
-from Utils.loader import env_variables
+from ..Utils.svg_manipulator import main as make_svg
+from ..Utils.loader import env_variables
 from werkzeug.datastructures import FileStorage
 
 
@@ -40,8 +40,16 @@ def audio_to_text(file_path: str) -> str:
 
 def extract_3_digit_numbers(text: str) -> list[int]:
 
-    pattern = r"\b\d{3}\b"
-    return [int(match) for match in re.findall(pattern, text)[:2]]
+    pattern_3_digit = r"\b\d{3}\b"
+    result = [int(match) for match in re.findall(pattern_3_digit, text)[:2]]
+
+    pattern_4_digit = r"\b2(\d{3})\b"
+    match_4_digit = re.search(pattern_4_digit, text)
+    if match_4_digit:
+
+        result.insert(1, int(match_4_digit.group(1)))
+
+    return result
 
 
 def main(
@@ -113,7 +121,6 @@ def extract_names(text):
 
     doc = nlp(text)
     names = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
-    print(names)
     return "".join(names)
 
 
@@ -121,19 +128,15 @@ def get_teacher_details_with_preprocessing(teacher_name, db_path):
 
     try:
 
-        print(teacher_name)
         teacher_name = re.sub(r"[^\w\s]", "", teacher_name)
         preprocessed_name_scypy = extract_names(teacher_name).lower()
         if not preprocessed_name_scypy or preprocessed_name_scypy in "kevin cabin":
             preprocessed_name_scypy = teacher_name.lower()
         preprocess_name_nltk = preprocess_query(teacher_name).lower()
-        print("nltk", preprocess_name_nltk)
-        print(preprocessed_name_scypy)
         common_chars = "".join(
             [char for char in preprocessed_name_scypy if char in preprocess_name_nltk]
         )
         preprocessed_name = "".join(common_chars)
-        print(preprocessed_name)
 
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
