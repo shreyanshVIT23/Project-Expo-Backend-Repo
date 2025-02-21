4ab9bda4a64edd0d289766a9c50b069712d74136
shreyanshVIT23 <shreyansh.23bce10701@vitbhopal.ac.in>
2025-02-21 14:16:58 +0530

    commiting for railway deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 4 -b 0.0.0.0: server.app:app
--- server/app.py
+++ server/app.py
@@ -231,4 +231,5 @@ def serve_assets(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
